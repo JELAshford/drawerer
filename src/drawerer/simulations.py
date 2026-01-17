ee2905ca5820d580073d983c1a4c810c45533ded
James Ashford <ashfordj@rocketmail.com>
2026-01-17 19:56:29 +0000

    Add random graph simulation example

--- src/drawerer/simulations.py
+++ src/drawerer/simulations.py
@@ -1,6 +1,8 @@
+from skimage.draw import line_aa, disk
 from drawerer.core import Simulation
 from scipy.signal import convolve2d
 from einops import repeat
+import networkx as nx
 import numpy as np
 
 
@@ -71,4 +73,49 @@ class RandomRainfall(Simulation):
             self.grid[*self.points, :] = 255
             # self.grid[..., 0] = np.roll(self.grid[..., 0], 1)
             # self.grid[..., 1] = np.roll(self.grid[..., 1], -1)
+            yield self.grid.astype(np.uint8)
+
+
+class RandomGraphSimulation(Simulation):
+    def __init__(
+        self,
+        grid_size: int = 1000,
+        num_points: int = 100,
+        num_connections: int = 80,
+        seed: int = 1701,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.grid_size = grid_size
+        self.rng = np.random.default_rng(seed=seed)
+        self.grid = np.zeros((self.grid_size, self.grid_size, 3))
+        # Create graph and get layout
+        self.G = nx.Graph()
+        for p1, p2, w in self.rng.integers(0, num_points, size=(num_connections, 3)):
+            self.G.add_edge(p1, p2, weight=w)
+        self.layout = nx.forceatlas2_layout(self.G)
+        self.layout = nx.rescale_layout_dict(pos=self.layout, scale=0.5)
+        self.layout = {
+            n: (p + 0.5) * (self.grid_size - 1) for n, p in self.layout.items()
+        }
+
+    def step(self, eps: float = 1e-6):
+        while True:
+            self.grid *= 0.1
+            # Generate layout
+            self.layout = nx.forceatlas2_layout(self.G, pos=self.layout)
+            self.layout = nx.rescale_layout_dict(pos=self.layout, scale=0.5)
+            self.layout = {
+                n: (p + 0.5) * (self.grid_size - 1) for n, p in self.layout.items()
+            }
+            # Draw graph edges
+            for ind1, ind2 in self.G.edges:
+                p1, p2 = self.layout[ind1], self.layout[ind2]
+                yy, xx, val = line_aa(*p1.astype(int), *p2.astype(int))
+                self.grid[yy, xx] = repeat((val * 0.5 * 255).astype(int), "l -> l 3")
+            # Draw nodes
+            for index, (y, x) in self.layout.items():
+                yy, xx = disk((y, x), 4, shape=self.grid.shape)
+                self.grid[yy, xx] = 255
+            # Update weights slightly
             yield self.grid.astype(np.uint8)
